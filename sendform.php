348df661d53a361e15b0978471b5d2f5669773c9
Pbaroque20 <raphaeltobar@gmail.com>
2026-02-02 21:13:39 -0500

    fix email

--- sendform.php
+++ sendform.php
@@ -15,29 +15,43 @@ header("refresh: 5; url=index.html");
 <body>
 
 <?php
-$to = "raphaeltobar@gmail.com";
+$to = "contact@tobarmediadesign.com";
 $firstname = '';
-$message = '';
+$lastname = '';
+$phonenumber = '';
 $email = '';
-if(isset($_POST['firstname'])){
-    $firstname = $_POST['firstname'];
-}
-if(isset($_POST['lastname'])){
-    $lastname = $_POST['lastname'];
-}
-if(isset($_POST['number'])){
-    $phonenumber = $_POST['number'];
-}
-if(isset($_POST['email'])){
-    $email = $_POST['email'];
-}
-if(isset($_POST['message'])){
-    $message = $_POST['message'];
-}
-$rtrnTo = $to;
+$message = '';
 
+if (isset($_POST['firstname'])) {
+    $firstname = trim($_POST['firstname']);
+}
+if (isset($_POST['lastname'])) {
+    $lastname = trim($_POST['lastname']);
+}
+if (isset($_POST['number'])) {
+    $phonenumber = trim($_POST['number']);
+}
+if (isset($_POST['email'])) {
+    $email = trim($_POST['email']);
+}
+if (isset($_POST['message'])) {
+    $message = trim($_POST['message']);
+}
 
-$mailSent = mail($to,$firstname . ' is interested in your service!',$message,$email,$rtrnTo);
+$subject = $firstname ? ($firstname . ' is interested in your service!') : 'Contact form submission';
+
+$body = "Name: " . $firstname . " " . $lastname . "\n";
+$body .= "Email: " . $email . "\n";
+$body .= "Phone: " . $phonenumber . "\n\n";
+$body .= "Message:\n" . $message;
+
+$headers = "Content-Type: text/plain; charset=utf-8\r\n";
+$headers .= "From: " . $to . "\r\n";
+if ($email) {
+    $headers .= "Reply-To: " . $email . "\r\n";
+}
+
+$mailSent = mail($to, $subject, $body, $headers);
 
 if($mailSent){
     echo "<div class='notification'>Your e-mail has been sent!</div>";
